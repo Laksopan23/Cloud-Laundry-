7f997f136c35fe6ca60cb33737280a88d146cf18
laksopan23 <laksopan2001@gmail.com>
2025-07-27 16:28:38 +0530

    feat: Implement code splitting to reduce bundle size

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -6,23 +6,23 @@ import {
   Navigate,
 } from "react-router-dom";
 
-import Dashboard from "./pages/common/Dashboard";
-import ForgotPassword from "./pages/forgotpassword";
-import OTPVerification from "./pages/OTPVerification";
-import ResetPassword from "./pages/ResetPassword";
-import PasswordResetSuccess from "./pages/PasswordResetSuccess";
-import Invoice from "./pages/orders/AddOrder";
-import ViewOrders from "./pages/orders/ViewOrders";
+const Dashboard = lazy(() => import("./pages/common/Dashboard"));
+const ForgotPassword = lazy(() => import("./pages/forgotpassword"));
+const OTPVerification = lazy(() => import("./pages/OTPVerification"));
+const ResetPassword = lazy(() => import("./pages/ResetPassword"));
+const PasswordResetSuccess = lazy(() => import("./pages/PasswordResetSuccess"));
+const Invoice = lazy(() => import("./pages/orders/AddOrder"));
+const ViewOrders = lazy(() => import("./pages/orders/ViewOrders"));
 
-import Profile from "./pages/profile/profile";
-import SignUp from "./pages/Auth/Employee/EmployeeSignup";
-import Login from "./pages/Auth/Employee/EmployeeLogin";
+const Profile = lazy(() => import("./pages/profile/profile"));
+const SignUp = lazy(() => import("./pages/Auth/Employee/EmployeeSignup"));
+const Login = lazy(() => import("./pages/Auth/Employee/EmployeeLogin"));
 
 //Admin
-import AllEmp from "./admin/allEmployee";
-import AdminDashboard from "./admin/AdminDashboard";
-import AdminSignUp from "./pages/Auth/Admin/sign";
-import AdminLogin from "./pages/Auth/Admin/login";
+const AllEmp = lazy(() => import("./admin/allEmployee"));
+const AdminDashboard = lazy(() => import("./admin/AdminDashboard"));
+const AdminSignUp = lazy(() => import("./pages/Auth/Admin/sign"));
+const AdminLogin = lazy(() => import("./pages/Auth/Admin/login"));
 
 function App() {
   // ProtectedRoute defined inside App.js
@@ -33,87 +33,89 @@ function App() {
 
   return (
     <Router>
-      <Routes>
-        {/* Public routes */}
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/pass" element={<ForgotPassword />} />
-        <Route path="/otp" element={<OTPVerification />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route
-          path="/reset-password-success"
-          element={<PasswordResetSuccess />}
-        />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {/* Public routes */}
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/pass" element={<ForgotPassword />} />
+          <Route path="/otp" element={<OTPVerification />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route
+            path="/reset-password-success"
+            element={<PasswordResetSuccess />}
+          />
 
-        {/* Protected routes */}
+          {/* Protected routes */}
 
-        {/* Admin routes */}
-        <Route
-          path="/all"
-          element={
-            <ProtectedRoute>
-              <AllEmp />
-            </ProtectedRoute>
-          }
-        />
+          {/* Admin routes */}
+          <Route
+            path="/all"
+            element={
+              <ProtectedRoute>
+                <AllEmp />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/admin-signup"
-          element={
-            <ProtectedRoute>
-              <AdminSignUp />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin-login"
-          element={
-            <ProtectedRoute>
-              <AdminLogin />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/dash"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute>
-              <AdminDashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/invoice"
-          element={
-            <ProtectedRoute>
-              <Invoice />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/orders"
-          element={
-            <ProtectedRoute>
-              <ViewOrders />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/prof"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+          <Route
+            path="/admin-signup"
+            element={
+              <ProtectedRoute>
+                <AdminSignUp />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin-login"
+            element={
+              <ProtectedRoute>
+                <AdminLogin />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dash"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <AdminDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/invoice"
+            element={
+              <ProtectedRoute>
+                <Invoice />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/orders"
+            element={
+              <ProtectedRoute>
+                <ViewOrders />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/prof"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
